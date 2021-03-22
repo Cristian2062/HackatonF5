e933b3e706e41f58c400d0b3e2bc9435af37eee5
martasanch <marta.sanch.ger@gmail.com>
2021-03-22 15:49:56 +0100

    añadida navbar

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import HomeAdmin from "../views/HomeAdmin.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/homeadmin",
+    name: "HomeAdmin",
+    component: HomeAdmin,
   },
   {
     path: "/about",
