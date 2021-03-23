baed50c8fbf3fda17617b230462262cad00c005a
Jairo <jairogarciachaves@gmail.com>
2021-03-23 10:03:03 +0100

    Finalizada vista de acceso

--- src/views/Acceso.vue
+++ src/views/Acceso.vue
@@ -1,10 +1,40 @@
 <template>
     <div class="acceso">
         <div class="usuario">
-            <img src="../public/user.png">
+        <a href=""> <img class="user" src="../assets/user.png"></a>
         </div>
         <div class="administrador">
-
+        <a href=""> <img class="admin" src="../assets/user-admin.png"></a>
+        <HelloAcceso/>
         </div>
     </div>
 </template>
+
+<style>
+.acceso{
+    display: flex;
+    justify-content: space-around;
+    padding-top: 20vh;
+}
+.user{
+    width: 30vw;
+    height: 30vh;
+}
+.admin{
+    width: 30vw;
+    height: 30vh;
+}
+@media only screen and (max-width: 500px){
+    .user{
+    width: 25vw;
+    height: 25vh;
+}
+    .admin{
+    width: 25vw;
+    height: 25vh;
+}
+}
+</style>
+
+
+
